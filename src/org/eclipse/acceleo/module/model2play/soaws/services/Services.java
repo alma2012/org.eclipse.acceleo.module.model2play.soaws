0345af3886687ab990ccb7be47748900539a6454
Neozaru <neozaru@mailoo.org>
2012-11-20 19:38:36 +0100

    First Commit, with Controller files generation
    Signed-off-by: Neozaru <neozaru@mailoo.org>

---
+++ src/org/eclipse/acceleo/module/model2play/soaws/services/Services.java
@@ -0,0 +1,18 @@
+package org.eclipse.acceleo.module.model2play.soaws.services;
+
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.obeonetwork.dsl.soa.Operation;
+import org.eclipse.acceleo.module.model2play.soaws.*;
+
+public class Services {
+
+
+	public void apply_operation_template( Operation anOperation ) {
+		
+
+		
+	}
+	
+};
